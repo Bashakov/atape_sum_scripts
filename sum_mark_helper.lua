4579c4ecb760b3caeda6460483a8c087562a0b5d
abashak <abashak@abisoft.biz>
2019-01-09 13:40:32 +0300

    fix Driver.GetGPS check

--- sum_mark_helper.lua
+++ sum_mark_helper.lua
@@ -760,7 +760,7 @@ local function MakeCommonMarkTemplate(mark)
 	row.RAIL_NAME = rails_names[row.RAIL_POS]
 	row.RAIL_TEMP = temperature and sprintf('%+.1f', temperature) or ''
 	
-	if Driver:GetGPS then
+	if Driver.GetGPS then
 		row.LAT, row.LON = Driver:GetGPS(prop.SysCoord)
 	else
 		row.LAT = ''
